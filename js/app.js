3a3f2b551001634547229a91d23c4e0a304d88f1
Daigo Fujiwara <daigo@daigofujiwara.com>
2019-03-09 16:00:25 -0500

    for presentation

--- js/app.js
+++ js/app.js
@@ -2,10 +2,8 @@ $(document).ready(function(){
   $(document).foundation();
 
   $.getJSON('js/2018-mlb-allplayers.json', function(players) {
-    console.log('test');
     console.log(players);
   });
-
 
 
   $('#team-table').DataTable({
